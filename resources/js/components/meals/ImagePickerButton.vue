3a79dcdd40e05242878bc3d07e4cb980237cb66a
Kevin <kevininc155@gmail.com>
2026-03-15 20:13:18 +0100

    [fix] linter

--- resources/js/components/meals/ImagePickerButton.vue
+++ resources/js/components/meals/ImagePickerButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref } from 'vue';
 import { Camera, X, ImagePlus } from 'lucide-vue-next';
 
 const emit = defineEmits<{
